5caae723b14f73392f8afc7b06814473def83617
gpoudel <gaurab.poudel@gmail.com>
2018-10-03 23:23:24 +0200

    update

--- js/app.js
+++ js/app.js
@@ -44,12 +44,13 @@
 
 	// User pool
 	var poolData = {
-			UserPoolId : 'eu-central-1_PxQpaQbgn', // Your user pool id here
-			ClientId : '5ut54i7nlf27ci0ec1g4tthg6k' // Your app client id here
+			UserPoolId : 'eu-central-1_3ZD4ClA6Q', // Your user pool id here
+			ClientId : '3akqatqq2j8ml3hp5v3lk7s7j0' // Your app client id here
 	};
 
 	// Your identity pool id here
 	var identityPoolId = "eu-central-1:37f885b2-0277-4f07-b4e5-358a6829d5a6"
+	
 
 	// Cognito Sync store name
 	var cognitoDatasetName = "footyVisison-users";
@@ -79,7 +80,7 @@
 					Value : $('#inputPreferredUsername').val()
 			},			
 			{
-					Name : 'custom:fpl_ID',
+					Name : 'custom:fpl_id',
 					Value : $('#inputFplId').val()
 			}
 		];
@@ -218,7 +219,7 @@ function updateProfile(){
 
 		var attributes = [			
 			{
-					Name : 'custom:fpl_ID',
+					Name : 'custom:fpl_id',
 					Value : $('#inputFplId2').val()
 			}
 		];
