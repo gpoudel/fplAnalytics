c3b91c40347c0b48f519836ad875d7eb14628f0a
gpoudel <gaurab.poudel@gmail.com>
2019-07-06 22:57:15 +0200

    update

--- js/downloadPlayerStatusData.js
+++ js/downloadPlayerStatusData.js
@@ -1,9 +1,9 @@
 function submitToAPI(e) {
      $.ajax({
       type: "POST",
-      url : "https://0e0c55ie39.execute-api.eu-central-1.amazonaws.com/default/fplAnalytics-DownloadPlayerStatusData",		 
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8"
+      url : "https://0e0c55ie39.execute-api.eu-central-1.amazonaws.com/default/fplAnalytics-DownloadPlayerStatusData"
+      //dataType: "json",
+     // crossDomain: "true",
+      //contentType: "application/json; charset=utf-8"
       });
   }
